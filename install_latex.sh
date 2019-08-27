2e8f6607b45c9f3680e047bc8036ae672f1b54a3
Alexander <alexander.thunnessen@gmx.com>
2019-08-27 22:06:58 +0200

    Update install_latex.sh

--- install_latex.sh
+++ install_latex.sh
@@ -2,4 +2,4 @@
 pkg install texlive
 tlmgr install babel babel-dutch # or babel-<your language>
 tlmgr install doublestroke hyperref paracol multicol multirow cancel enumitem
-tlmgr install graphicx float todonotes pgf tikz-cd xcolor
+tlmgr install graphicx float todonotes pgf tikz-cd xcolor mathtools
