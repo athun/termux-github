7bb7417742089fae2da147b49c0c04fa71d8b02a
Alexander <alexander.thunnessen@gmx.com>
2019-09-10 21:32:11 +0200

    add "caption" package to latex installation
    "caption" contains subcaption

--- install_latex.sh
+++ install_latex.sh
@@ -1,5 +1,5 @@
 #/usr/bin/bash
 pkg install texlive
 tlmgr install babel babel-dutch # or babel-<your language>
-tlmgr install doublestroke hyperref paracol multicol multirow cancel enumitem
+tlmgr install doublestroke hyperref paracol multicol multirow cancel enumitem caption
 tlmgr install graphicx float todonotes pgf tikz-cd xcolor mathtools wrapfig was
