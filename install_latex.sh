2ae78deebb579abd50b9db84067b4eba81d8b5b6
Alexander <alexander.thunnessen@gmx.com>
2019-10-17 23:27:59 +0200

    remove mnsymbol

--- install_latex.sh
+++ install_latex.sh
@@ -2,4 +2,4 @@
 pkg install texlive
 tlmgr install babel babel-dutch # or babel-<your language>
 tlmgr install doublestroke hyperref paracol multicol multirow cancel enumitem caption
-tlmgr install graphicx float todonotes pgf tikz-cd xcolor mathtools wrapfig was MnSymbol
+tlmgr install graphicx float todonotes pgf tikz-cd xcolor mathtools wrapfig was
